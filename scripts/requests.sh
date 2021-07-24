415a81020b3b0a40f0113c2c8cd6ea1b42e6cd2e
ahyaemon <iamahyaemon@gmail.com>
2021-07-24 15:58:48 +0900

    express-rate-limit

---
+++ scripts/requests.sh
@@ -0,0 +1,9 @@
+curl http://localhost:3000
+echo ""
+curl http://localhost:3000
+echo ""
+curl http://localhost:3000
+echo ""
+curl http://localhost:3000
+echo ""
+curl http://localhost:3000
--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 3,
+});
+
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
